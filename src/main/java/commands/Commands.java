0463aa5d3b0cd49ed5f407928437c989a56ad5f4
Mitchell Dennett <mitch@mylodevelopment.com>
2020-08-24 20:13:37 -0400

    Added make command & make controller commands

--- src/main/java/commands/Commands.java
+++ src/main/java/commands/Commands.java
@@ -1,15 +1,14 @@
 package {{.Package}}.commands;
 
-import com.mitchdennett.framework.commands.Command;
-import com.mitchdennett.framework.commands.MakeMigration;
-import com.mitchdennett.framework.commands.MigrateCommand;
-import com.mitchdennett.framework.commands.MigrateRollbackCommand;
+import com.mitchdennett.framework.commands.*;
 
 public class Commands {
 
     public static final Class<Command>[] COMMANDS = new Class[]{
             MakeMigration.class,
             MigrateCommand.class,
-            MigrateRollbackCommand.class
+            MigrateRollbackCommand.class,
+            MakeCommand.class,
+            MakeController.class
     };
 }
